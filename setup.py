ded5f26aeadc74664e0b5155da9fda3268bcd205
Dominik Miedziński <dominik@mdzn.pl>
2018-10-01 11:56:38 +0200

    Use pipenv for package management

--- setup.py
+++ setup.py
@@ -1,15 +1,8 @@
 from setuptools import find_packages, setup
 
-
-def read_requirements():
-    with open('requirements.txt') as f:
-        return f.read().splitlines()
-
-
 setup(
     name='nnc',
     packages=find_packages(),
-    install_requires=read_requirements(),
     entry_points={
         'console_scripts': [
             'nnc=nnc.main:main'
